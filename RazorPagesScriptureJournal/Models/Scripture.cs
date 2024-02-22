fc47b6001e0b826a5fae72f9bbb1c282878b9639
luke bullock <111459215+TrueGlimGrold@users.noreply.github.com>
2024-02-22 16:54:59 -0700

    I'm going to have to start over again

---
+++ RazorPagesScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string StandardWork { get; set; } = string.Empty;
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Book { get; set; } = string.Empty;
+
+        [StringLength(60, MinimumLength = 0)]
+        [Required]
+        public string Chapter { get; set; } = string.Empty;
+
+        [StringLength(60, MinimumLength = 0)]
+        public string Verse { get; set; } = string.Empty;
+
+        [StringLength(600, MinimumLength = 3)]
+        public string JounralEntry { get; set; } = string.Empty;
+
+        [StringLength(120, MinimumLength = 0)]
+        public string Keywords { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+    }
+}
